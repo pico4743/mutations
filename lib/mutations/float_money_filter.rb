9601b43d3b38eaa53641eeb9dfcf421e380bd84e
pico4743 <pconner@lightfootwebtools.com>
2014-02-14 15:45:50 -0500

    add proper initialization of defaults to float money filter

--- lib/mutations/float_money_filter.rb
+++ lib/mutations/float_money_filter.rb
@@ -1,5 +1,15 @@
 module Mutations
   class FloatMoneyFilter < FloatFilter
+    @default_options = {
+      :nils => false,       # true allows an explicit nil to be valid. Overrides any other options
+      :empty => true,       # true allows empty string, false rejects empty strings as non-numeric
+      :min => nil,          # lowest value, inclusive
+      :max => nil           # highest value, inclusive
+    }
+
+    def initialize(opts = {})
+      self.options = (self.class.default_options || {}).merge(opts)
+    end
 
     def filter(data)
       if data.is_a?(String)
